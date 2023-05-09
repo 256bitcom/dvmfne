6897c8f01fc09cf936b8eb79858c518007ee43dc
256bitcom <127780877+256bitcom@users.noreply.github.com>
2023-05-09 14:07:39 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-echo "Beginning Setup"
-sudo bash
-python3.9 -m venv . && source bin/activate
-pip install wheel && pip install -r requirements.txt
-cd monitor 
-pip install wheel && pip install -r requirements.txt
-cd ..
-echo "Setup Complete"
