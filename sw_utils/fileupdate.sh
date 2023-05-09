d54c27c4a36c536c1a31c462d17b39b5502cf56c
256bitcom <127780877+256bitcom@users.noreply.github.com>
2023-05-09 14:12:28 -0400

    Update fileupdate.sh

--- sw_utils/fileupdate.sh
+++ sw_utils/fileupdate.sh
@@ -1,1 +1,11 @@
-
+sudo bash
+echo "Moving and renaming files!"
+echo off
+cd /opt/dvmfne/
+cd cfgs
+mv fne_SAMPLE.cfg /opt/dvmfne/fne/fne.cfg
+mv fne_router-SAMPLE.cfg /opt/dvmfne/cfg/fne_router.cfg
+mv fne_parrot-SAMPLE.cfg /opt/dvmfne/cfg/fne_parrot.cfg
+mv fne_bridge-SAMPLE.cfg /opt/dvmfne/cfg/fne_bridge.cfg
+cd /opt/dvmfne/
+echo "Operation Complete!"
