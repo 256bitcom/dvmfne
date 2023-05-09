006c5fe27572781806d8824380021dfa89ac3893
256bitcom <127780877+256bitcom@users.noreply.github.com>
2023-05-09 14:13:13 -0400

    Update setup.sh

--- sw_utils/setup.sh
+++ sw_utils/setup.sh
@@ -1,8 +1,10 @@
-echo "Beginning Setup"
 sudo bash
+echo "Beginning Setup"
+echo off
 python3.9 -m venv . && source bin/activate
 pip install wheel && pip install -r requirements.txt
 cd monitor 
 pip install wheel && pip install -r requirements.txt
 cd ..
 echo "Setup Complete"
+exit
